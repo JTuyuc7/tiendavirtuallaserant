42ef6c7e0a39346ee28e5d1bcd4b59dddb8f5761
james tuyuc <jaimetuyuc7@gmail.com>
2022-03-07 00:29:43 -0600

    Navigation Added
    Navigation config added

---
+++ src/components/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+
+    return(
+        <>
+            <div>
+                <h1>Prueba Iniciar sesion</h1>
+            </div>
+        </>
+    )
+}
+
+export default Login;
---
+++ src/components/pages/MyOrders.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const MyOrders = () => {
+
+    return(
+
+        <>
+            <div>
+                <h2>From My Orders</h2>
+            </div>
+        </>
+    )
+}
+
+export default MyOrders;
