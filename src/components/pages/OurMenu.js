8233626701f1d8471854709d9e9976d836539ca7
james tuyuc <jaimetuyuc7@gmail.com>
2022-04-23 19:34:40 -0600

    fixes for deploy

--- src/components/pages/OurMenu.js
+++ src/components/pages/OurMenu.js
@@ -21,6 +21,7 @@ const OurMenu = () => {
     // Get Data
     useEffect(() => {
         dispatch(getAllDishesAction())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
     const toogleVisivility = () => {
@@ -69,7 +70,7 @@ const OurMenu = () => {
                             </select>
                         </div>
                     <div className='mt-5 mb-5'>
-                        <h1 className='text-center font-bold uppercase text-2xl mb-5'>{ search && filteredData == 0  ? `No dishes for ${search} founded` : dishes.length === 0 ? 'No dishes added, start adding one' : 'Our Menu'  }</h1>
+                        <h1 className='text-center font-bold uppercase text-2xl mb-5'>{ search && filteredData === 0  ? `No dishes for ${search} founded` : dishes.length === 0 ? 'No dishes added, start adding one' : 'Our Menu'  }</h1>
                         <div className='sm:grid-cols-2 sm:grid gap-6 xl:grid-cols-3'>
                             { filteredData?.map( (dish) => (
                                 <Dish 
